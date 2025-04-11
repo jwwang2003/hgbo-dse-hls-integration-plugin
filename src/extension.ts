3634da389f44175d0131547f7d70e125b5da4d1a
JUN WEI WANG <wjw_03@outlook.com>
2025-04-11 17:43:45 +0800

    Added custom webview component, future: use svelte to build multiple webviews with better modularization

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { FlowPanel } from './panels/FlowPanel';
+import { SidebarProvider } from './SidebarProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// Debug welcome message
+	console.log('Compass HLS VSC extension is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hgbo-dse-hls-integration-plugin" is now active!');
+	// Register the sidebar panel
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"compass-flow",
+			sidebarProvider
+		)
+	);
+
+	// Load commands
+	const showHelloWorldCommand = vscode.commands.registerCommand("hello-world.showHelloWorld", () => {
+    FlowPanel.render(context.extensionUri);
+  });
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
---
+++ webview-ui/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="p-2 flex flex-col justify-center gap-2">
+      <button className="button">Generate TDM</button>
+      <button className="button">Run HGBO</button>
+      <button className="button">View Graphs</button>
+      <button className="button">Export</button>
+    </div>
+  )
+}
+
+export default App
